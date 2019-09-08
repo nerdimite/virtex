043e3ff5f7be14345a45b14dc56405dbd77a3965
Nerdimite37 <bhavesh.laddagiri1@gmail.com>
2019-09-08 13:43:49 +0530

    reorganized

--- yoga_example.py
+++ yoga_example.py
@@ -9,7 +9,7 @@ from utils import combine_poses, save_obj
 An Example Program to train on Yoga Poses
 '''
 
-dataset = combine_poses('.yoga_example/data/plank-0.csv','.yoga_example/data/tree-0.csv')
+dataset = combine_poses('./yoga_example/data/plank-0.csv','./yoga_example/data/tree-0.csv')
 x_train, x_test, y_train, y_test, classes = preprocess_data(dataset)
 
 classifier = train_classifier(x_train, y_train, 'yoga')
@@ -17,4 +17,4 @@ classifier = train_classifier(x_train, y_train, 'yoga')
 acc_mean, acc_std = cross_val(classifier,x_train,y_train)
 print(acc_mean,acc_std)
 
-save_obj(classes, '.yoga_example/classifiers/yoga.cls')
+save_obj(classes, './yoga_example/classifiers/yoga.cls')
